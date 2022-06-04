71994ed4e88ce44cdf59d4dbb4671c9b5835ca16
shubhi182001 <kediashubhi@gmail.com>
2022-06-04 15:59:24 +0530

    added dropdown in navbar

---
+++ src/Pages/Admin/homeBox/Homebox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+
+function Homebox() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default Homebox
--- src/Pages/Admin/navbar/Navbar.js
+++ src/Pages/Admin/navbar/Navbar.js
@@ -17,8 +17,26 @@ function Navbar() {
        <div className='right'>
           <div className="navbarRight">
           <div className="buttonWrapper">
-            <button className=" but get"><p className='buttonText'>GET</p></button>
-            <button className=' but add'><p className='buttonText'>ADD</p></button>
+            <div class="but dropdown">
+              <button class="dropbtn">GET</button>
+              <div class="dropdown-content">
+                <a href="/">Get Questions</a>
+                <hr style={{color:"white"}}/>
+                <a href="/">Get Feedback Questions</a>
+                <hr style={{color:"white"}}/>
+                <a href="/">Get Candidates</a>
+              </div>
+            </div>
+            <div class="but dropdown">
+              <button class="dropbtn">ADD</button>
+              <div class="dropdown-content">
+                <a href="/">Add Questions</a>
+                <hr style={{color:"white"}}/>
+                <a href="/">Add Feedback Questions</a>
+                <hr style={{color:"white"}}/>
+                <a href="/">Add Candidates</a>
+              </div>
+            </div>
             <button className="but leaderboard"><p className='buttonText'>LEADERBOARD</p></button>
             <FiPower className="but logoutBtn" style={{color:'white', marginTop:"22px", fontSize:"21px" }}/>
           </div>
