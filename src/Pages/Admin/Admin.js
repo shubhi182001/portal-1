c197224d2d0d8d7b6c282878ba2ca770369f2a2d
shubhi182001 <kediashubhi@gmail.com>
2022-06-04 17:16:25 +0530

    resolved navbar css

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './navbar/Navbar'
+import "./Admin.css"
+import { Card } from '@mui/material'
 
 function Admin() {
   return (
     <>
     <Navbar/>
+    <div className='card'>
+    <Card className='card-content'>
+      hello
+    </Card>
+    </div>
+    
     </>
   )
 }
