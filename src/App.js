8ccb73fe44cb248644e13262da61b5e1319aff28
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-06-05 22:19:27 +0530

    validations on login page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       {/* <Feedback /> */}
-      {/* <Login/> */}
-      <Admin/>
+      <Login/>
+      {/* <Admin/> */}
     </div>
   );
 }
