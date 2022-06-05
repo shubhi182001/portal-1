68be86921f7aa202761fbe48388c3b8e2ae34698
1999matrix <shashwatsd44@gmail.com>
2022-06-06 02:24:12 +0530

    added animation

--- src/Pages/User/User.js
+++ src/Pages/User/User.js
@@ -2,17 +2,19 @@ import './User.css';
 import React from 'react';
 import Feedback from './Feedback/Feedback';
 import Login from './Login/Login';
+import Animation from './Animation/Animation';
 import Instructions from './Instruction/Instruction'
 import Thankyou from './Thankyoupage/Thankyou';
 import Confirmation from './Confirmationpage/Confirmation';
 function App() {
   return (
     <div className="App">
-      <Confirmation />
-      {/* <Feedback /> */}
+    <Animation/>
+  {  /*  <Confirmation /> */ }
+      {  /*  <Feedback /> */ }
       {/* <Thankyou/> */}
-      {/* <Login/> */}      
-      {/* <Instructions/> */}
+      {/*<Login/>*/ }      
+      {/*<Instructions/>*/} 
     </div>
   );
 }
