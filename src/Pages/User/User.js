724d72e3d377002525a1593722b36014dfcbd183
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-06-06 17:46:49 +0530

    removed animation and background blue

--- src/Pages/User/User.js
+++ src/Pages/User/User.js
@@ -2,19 +2,17 @@ import './User.css';
 import React from 'react';
 import Feedback from './Feedback/Feedback';
 import Login from './Login/Login';
-import Animation from './Animation/Animation';
 import Instructions from './Instruction/Instruction'
 import Thankyou from './Thankyoupage/Thankyou';
 import Confirmation from './Confirmationpage/Confirmation';
 function App() {
   return (
     <div className="App">
-    <Animation/>
   {  /*  <Confirmation /> */ }
       {  /*  <Feedback /> */ }
       {/* <Thankyou/> */}
-      {/*<Login/>*/ }      
-      {/*<Instructions/>*/} 
+       <Login/>       
+      {/* <Instructions/>  */}
     </div>
   );
 }
