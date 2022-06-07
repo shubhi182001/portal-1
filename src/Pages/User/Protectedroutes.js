6d6222212c908205ab641cafbbd61e1afab0846f
vratant11 <vratant2012061@gmail.com>
2022-06-07 09:09:25 +0530

    Protected Login and Instruction page

--- src/Pages/User/Protectedroutes.js
+++ src/Pages/User/Protectedroutes.js
@@ -2,7 +2,7 @@ import React, { Component, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Protectedroutes = (props) => {
-    const {Cmp} = props;
+    const {Component} = props;
     const navigate= useNavigate();
     useEffect(()=>
     {
