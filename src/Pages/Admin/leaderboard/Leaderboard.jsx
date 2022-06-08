a3a749f2d1eb3c75871276697b0442f810df070e
SaraswatAnushka <saraswatanushka007@gmail.com>
2022-06-08 18:59:22 +0530

    Leaderboard addition

--- src/Pages/Admin/leaderboard/Leaderboard.jsx
+++ src/Pages/Admin/leaderboard/Leaderboard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
+import "./leaderboard.css"
+import { TextField } from '@mui/material'
+
+
 
 const Leaderboard = () => {
   return (
-    <div>Leaderboard</div>
+    <>
+    <Navbar/>
+    <div className='dabba'>
+    <div className='top'>
+    Leaderboard </div>
+    </div>
+    <div className='wrap'>
+      
+      <div className="search">
+        <TextField id="outlined-basic"
+        variant='outlined'
+        fullWidth
+        label="Search"
+        />
+      </div>
+    </div>
+    </>
   )
 }
 
