adcbf0edb234d6a79cf34dec12698665f6559ba1
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-06-10 18:46:40 +0530

    axios merged

--- src/Pages/User/Testwindow/components/Sidebar.js
+++ src/Pages/User/Testwindow/components/Sidebar.js
@@ -1,36 +1,10 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import './Sidebar.css'
-=======
 import React from "react";
 import "./Sidebar.css";
->>>>>>> 9b98a66294853178af39770bfd736be415e2a9d7
 const Sidebar = () => {
   const [hours,setHours]= useState();
   const [minutes,setMinutes]= useState();
   const [seconds,setSeconds]= useState();
   return (
-<<<<<<< HEAD
-    <div className='Sidebar_body'>
-      <div></div>
-     <div className="time_left">Time left</div>
-     <div className="time_section">11 : 00 : 00</div>
-     <div className="heading">Questions</div>
-     <div className="buttons_container">
-       <div className="buttons">
-         <button className='sidebar_button'>1</button>
-         <button>1</button>
-         <button>1</button>
-         <button>1</button>
-         <button>1</button>
-         <button>1</button>
-       </div>
-       <button className='submit_button'>Submit Test</button>
-     </div>
-    </div>
-  )
-}
-=======
     <div className="Sidebar_body">
       <div className="time_left">
         <h1>Time left</h1>
@@ -50,7 +24,6 @@ const Sidebar = () => {
         </div>
       </div>
         <div className="time_head">Questions</div>
->>>>>>> 9b98a66294853178af39770bfd736be415e2a9d7
 
       <div className="text-wrapper">
         <div className="buttons">
