b51e53290f3015f98043ac86284bc20b53cf87a0
shubhi182001 <kediashubhi@gmail.com>
2022-06-11 18:21:06 +0530

    edit questions page ui done

--- src/Pages/Admin/editQ/components/OptionList.js
+++ src/Pages/Admin/editQ/components/OptionList.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Checkbox from '@mui/material/Checkbox';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+
 
 function OptionList({optionL, setOptionL}) {
   return (
     <div>
         {optionL.map((option)=>(
             <li className='option-list'>
-                <input type="text" value={option.title} className="list" onChange={(e) => e.preventDefault()} />
+              <div className="input-list">
+                <Checkbox/>
+                <input type="text" value={option.title} className="opt-list" onChange={(e) => e.preventDefault()} />
+                <DeleteIcon style={{color:"#DE5947", marginLeft :"60%", cursor:"pointer"}}/>
+              </div>
+                {/* <div>
+                <FormGroup>
+                  <FormControlLabel  control={<Checkbox /> }  className="opt-list" onChange={(e) => e.preventDefault()}   label={option.title}  />
+                </FormGroup>
+                </div> */}
             </li>
         ))}
     </div>
