fbcf6d80373d046dd7d88b8b0f77a120c3ed94cc
Amit Singh <amitsinghjin13@gmail.com>
2022-06-17 10:16:01 -0700

    api in addques

--- src/Pages/Admin/add/addQuestions/AddQuestions.js
+++ src/Pages/Admin/add/addQuestions/AddQuestions.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import './addquestions.css'
+import axios from "axios";
 
 
 
 const AddQuestions = () => {
     const [inputText, setInputText] = useState('');
     const [options, setOptions] = useState([]);
-
+    const [chosenlang, setChosenlang] = useState("");
+    const [question, setQuestion] = useState("");
 
 
     const inputTextHandler = (e) => {
@@ -16,12 +18,43 @@ const AddQuestions = () => {
     const submitHandler = (e) => {
         e.preventDefault();
         setOptions(
-            [...options, { text: inputText, isRight: false, id: Math.random() * 1000 }]
+            [...options, { value: inputText, Oid: Math.random() * 1000, isCorrect: false }]
         );
         setInputText(' ');
     }
 
+    const handleLanguage = (e) => {
+        e.preventDefault();
+        setChosenlang(e.target.value)
+        // console.log(e.target.value)
+    }
 
+    const handleQuestion = (e) => {
+        e.preventDefault();
+        setQuestion(e.target.value)
+        // console.log(e.target.value)
+    }
+    const handleUpload = (e) => {
+        e.preventDefault();
+        console.log('Uploaded')
+        const questionData = [
+            {
+
+                "question": question,
+                "category": String(chosenlang),
+                "options": options
+
+            },
+        ]
+        console.log(questionData)
+        axios.post('https://csiportal.herokuapp.com/question/addquestion', questionData)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
 
     console.log(options);
     return (
@@ -32,11 +65,19 @@ const AddQuestions = () => {
                     <div className="questions">
                         <div className="question">
                             <p>Question</p>
-                            <textarea name="ques" id="question-here" >the question comes here </textarea>
+                            <textarea name="ques" id="question-here" onChange={handleQuestion}>the question comes here </textarea>
                         </div>
                     </div>
                     <div className="dropdown">
-                        languages dropdown here
+                        <div className="lang">
+                            <select className="select" defaultValue={"DEFAULT"} style={{ color: "white" }} onChange={handleLanguage} name="lang" id="options" >
+                                <option value="DEFAULT" disabled hidden>Language</option>
+                                <option value="C" style={{ color: "black", backgroundColor: "#F6FCFF" }}>C</option>
+                                <option value="C++" style={{ color: "black", backgroundColor: "#F6FCFF" }}>C++</option>
+                                <option value="Java" style={{ color: "black", backgroundColor: "#F6FCFF" }}>Java</option>
+                                <option value="Python" style={{ color: "black", backgroundColor: "#F6FCFF" }}>Python</option>
+                            </select>
+                        </div>
                     </div>
                     <div className="answers">
                         <div className="answers-and-options">
@@ -56,21 +97,20 @@ const AddQuestions = () => {
                                     <ul className="option-list">
                                         {options.map(option => (
                                             <div className="option">
-                                                {/* <input type="checkbox" name="" id="" /> */}
                                                 <button className="s-class item-1 check" onClick={() => {
                                                     setOptions(options.map((el) => {
-                                                        if (el.id === option.id) {
+                                                        if (el.Oid === option.Oid) {
                                                             return {
-                                                                ...el, isRight: !el.isRight
+                                                                ...el, isCorrect: !el.isCorrect
                                                             }
                                                         }
                                                         return el;
                                                     }))
 
                                                 }}>?</button>
-                                                <li className="s-class item-2 option-item">{option.text}</li>
+                                                <li className="s-class item-2 option-item">{option.value}</li>
                                                 <button className="s-class item-3  delete" onClick={() => {
-                                                    setOptions(options.filter(el => el.id !== option.id))
+                                                    setOptions(options.filter(el => el.Oid !== option.Oid))
                                                     console.log(options);
                                                 }}>*</button>
 
@@ -82,8 +122,14 @@ const AddQuestions = () => {
                         </div>
 
                     </div>
+                    <div className="upload-button">
+                        <button onClick={handleUpload}>Upload</button>
+                    </div>
                 </div>
+
+
             </div>
+
         </>
     )
 }
