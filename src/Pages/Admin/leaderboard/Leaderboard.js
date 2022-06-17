d18a4e5385b5f128c220a929c0a680c44f5fff52
tanya172 <tannyaverma086@gmail.com>
2022-06-17 17:58:58 +0530

    leaderboard css

--- src/Pages/Admin/leaderboard/Leaderboard.js
+++ src/Pages/Admin/leaderboard/Leaderboard.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import Navbar from '../navbar/Navbar'
-import "./leaderboard.css"
-import { TextField } from '@mui/material'
-
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import "./leaderboard.css";
 
 
 const Leaderboard = () => {
   return (
     <>
-    <Navbar/>
-    <div className='dabba'>
-    <div className='top'>
-    Leaderboard </div>
-    </div>
-    <div className='wrap'>
-      
-      <div className="search">
-        <TextField id="outlined-basic"
-        variant='outlined'
-        fullWidth
-        label="Search"
-        />
+      <Navbar />
+      <div className="leader">
+        <div className="top">Leaderboard </div>
       </div>
-    </div>
+     
+     <div className="inputwrap">
+     <input className="wrap" type="text" placeholder="Search..."></input>
+     </div>
+      
+      <div className="box3">
+        <div className="content">
+          <div className="details">
+            <div className="information">
+              
+            </div>
+          </div>
+        </div>
+        </div>
+      
     </>
-  )
-}
+  );
+};
 
 export default Leaderboard;
