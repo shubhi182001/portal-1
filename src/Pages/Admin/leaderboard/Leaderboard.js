770294f1ddce6e5904afbc702243a45c76d1a3a8
tanya172 <tannyaverma086@gmail.com>
2022-06-27 17:22:48 +0530

    leaderboard

--- src/Pages/Admin/leaderboard/Leaderboard.js
+++ src/Pages/Admin/leaderboard/Leaderboard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+// import { Button } from "bootstrap";
 import "./leaderboard.css";
 
 
-const Leaderboard = () => {
+function Leaderboard() {
   return (
     <>
-    <div className="admin-main">
       <Navbar />
       <div className="leader">
         <div className="top">Leaderboard </div>
@@ -20,12 +20,42 @@ const Leaderboard = () => {
         <div className="content">
           <div className="details">
             <div className="information">
+            <div className="info_ques">
+            <ul>
+                <li>Name</li>
+                <li>Student no.</li>
+                <li>Branch</li>
+                <li>Score</li>
+                <li>Start Time</li>
+                <li>End Time</li>
+                <div className="fetch">
+                <div className="fetch_details">Fetch Details</div>
+                </div>
+            </ul>
+            </div>
+            <div className="info_ans">
+            <ul>
+                <li>Nate</li>
+                <li>2010565</li>
+                <li>CSE</li>
+                <li>100</li>
+                <li>10:00</li>
+                <li>12:00</li>
+                <div className="answers">
+                <div className="fetch_answers">
+                Fetch Answers</div>
+                </div>
+            </ul>
+            </div>
+           
+            </div>
+            </div>
+        </div>
               
             </div>
-          </div>
-        </div>
-        </div>
-        </div>
+          
+      
+
       
     </>
   );
---
+++ src/Pages/Admin/leaderboard/details.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../navbar/Navbar";
+
+function details() {
+
+  
+    return(
+        <> <Navbar />
+        </>
+    ) ;
+  };
+
+  export default details;
+  
