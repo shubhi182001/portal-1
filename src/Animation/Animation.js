c04d05195e1501b507e9b629a3749658cef0d37b
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-07-11 01:36:10 +0530

    context removed

--- src/Animation/Animation.js
+++ src/Animation/Animation.js
@@ -15,7 +15,7 @@ const Animation = () => {
     <body id='anime' className='body-i'>
     <div id='ani'>
     <div className='anime-2'>
-    <img src={logo} alt="not found" className='logo-new' onload="document.body.style.opacity='0'"></img>
+    <img src={logo} alt="not found" className='logo-new' onLoad="document.body.style.opacity='0'"></img>
     </div>
     </div>
     </body>
