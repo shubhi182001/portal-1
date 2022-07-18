8ec87d212c2be5bb729bf52d517644d4087c41a9
shubhi182001 <kediashubhi@gmail.com>
2022-07-18 23:51:16 +0530

    css changes

--- src/Pages/Admin/Get/GetCandidate/GetCandidate.js
+++ src/Pages/Admin/Get/GetCandidate/GetCandidate.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../navbar/Navbar'
 
 const GetCandidate = () => {
   return (
-    <div className='admin-main'>
+    <div className='admin-main getc'>
+      <Navbar/>
       GetCandidate
     </div>
   )
