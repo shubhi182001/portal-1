bcc9b294309e7ff11818e76f4ddb7043597b45bd
shubhi182001 <kediashubhi@gmail.com>
2022-07-24 15:07:06 +0530

    ui changes in add question

--- src/Pages/Admin/add/addQuestions/AddQuestions.js
+++ src/Pages/Admin/add/addQuestions/AddQuestions.js
@@ -98,7 +98,7 @@ const AddQuestions = () => {
     console.log(options);
     return (
         <>
-            <div className="admin-main">
+            <div className="admin-main add-main">
                 <Navbar />
                 <div className='add-question-body'>
                     <h5 className='heading-question'>Add Question </h5>
@@ -127,8 +127,8 @@ const AddQuestions = () => {
                         <div className="answers-and-options">
                             <div className="answers-section">
                                 <p>Options</p>
-                                <form action="" onSubmit={submitHandler}>
-                                    <div className="answer-input-field">
+                                <form action="" onSubmit={submitHandler} >
+                                    <div className="answer-input-field" >
                                         <input value={inputText}
                                             onChange={inputTextHandler} type="text" name="" id="" placeholder='Add Options' />
                                     </div>
@@ -137,7 +137,7 @@ const AddQuestions = () => {
                             <div className="options-section">
                                 <ul className="option-list">
                                     {options.map(option => (
-                                        <div className="option">
+                                        <div className="option" style={{alignItems:"center"}}>
                                             <Checkbox checked={option.isCorrect} value={option.value} className="s-class item-1 check" onClick={() => {
                                                 setOptions(options.map((el) => {
                                                     if (el.Oid === option.Oid) {
@@ -150,7 +150,7 @@ const AddQuestions = () => {
 
                                             }} />
                                             <li className="s-class item-2 option-item">{option.value}</li>
-                                            <DeleteIcon fontSize="small" style={{ color: "#DE5947", cursor: "pointer" }} className="s-class item-3  delete" onClick={() => {
+                                            <DeleteIcon fontSize='10px' height="8px"  style={{ color: "red", cursor: "pointer" }} className=" item-3  delete" onClick={() => {
                                                 setOptions(options.filter(el => el.Oid !== option.Oid))
                                                 // console.log(options)
                                             }} />
@@ -177,4 +177,3 @@ const AddQuestions = () => {
 }
 
 export default AddQuestions
-
