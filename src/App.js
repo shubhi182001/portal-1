7cc398d3bc284edc1795a5147857708bd7b02048
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-08-22 12:49:46 +0530

    mobile view toast container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import React from "react";
 import User from "./Pages/User/User";
+import { ToastContainer, toast } from "react-toastify";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  toast.error("Sorry, won't open in phones");
   return (
-    window.innerWidth > "820" ? <User /> : <h1 className="mobile_view"></h1>
-
+    window.innerWidth > "820" ? <User /> : <div className="mobile_view"> <ToastContainer/></div> 
       
   );
 }
