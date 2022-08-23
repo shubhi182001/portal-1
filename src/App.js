3b0e981294489a52e5889c38998d894eb18cd204
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-08-23 11:36:25 +0530

    desktop restriction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import User from "./Pages/User/User";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MobileError from "./Error/MobileError";
 
 function App() {
-  return window.innerWidth > "820" ? <User /> : <MobileError />;
+  const [mobile, setMobile] = useState(true);
+  useEffect(() => {
+    if (window.matchMedia('(orientation:portrait)').matches) {
+      // eslint-disable-next-line
+       setMobile(false);
+    }
+    else if(window.innerWidth < "820" )
+    {
+      setMobile(false);
+    }
+    console.log(mobile);
+  }, []);
+
+  return mobile === true ? <User /> : <MobileError />;
 }
 
 export default App;
